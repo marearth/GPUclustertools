7a3daacbcea1db460704170919436a7f8b1b684d
marearth <niezhiwen.nie@gmail.com>
2021-11-05 17:23:15 +0800

    add installation of job_by_user

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ if [ -f "${HOME}/.bash_aliases" ];then
     echo "alias ${gen_pbs_sh}=\"${install_path}/gen_pbs.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${gen_slm_sh}=\"${install_path}/gen_slm.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${job_by_name_sh}=\"${install_path}/job_by_name.sh\"" >> ${HOME}/.bash_aliases
+    echo "alias ${job_by_user_sh}=\"${install_path}/job_by_user.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${gpu_summary_sh}=\"${install_path}/gpu_summary.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${sbatchd_sh}=\"${install_path}/sbatchd.sh\"" >> ${HOME}/.bash_aliases
 else
@@ -28,6 +29,7 @@ else
     echo "alias ${gen_pbs_sh}=\"${install_path}/gen_pbs.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${gen_slm_sh}=\"${install_path}/gen_slm.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${job_by_name_sh}=\"${install_path}/job_by_name.sh\"" >> ${HOME}/.bash_aliases
+    echo "alias ${job_by_user_sh}=\"${install_path}/job_by_user.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${gpu_summary_sh}=\"${install_path}/gpu_summary.sh\"" >> ${HOME}/.bash_aliases
     echo "alias ${sbatchd_sh}=\"${install_path}/sbatchd.sh\"" >> ${HOME}/.bash_aliases
 fi
